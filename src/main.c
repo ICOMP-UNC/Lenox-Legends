e594e1840ff381d53a1662eec650e83727c67962
franivan98 <franko15mamani@gmail.com>
2024-11-09 01:21:54 -0300

    Libreria RTOS agregada
    se agrego el directorio con la libreria rtos, y se cambio el contenido de main para realizar un codigo de prueba usando free RTOS

--- src/main.c
+++ src/main.c
@@ -1,18 +1,41 @@
-#include <libopencm3/stm32/gpio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include <stdio.h>
+
 #include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+#define tskLOW_PRIORITY ((UBaseType_t)tskIDLE_PRIORITY+2)
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask __attribute__((unused)), char *pcTaskName __attribute__((unused))){
+    while (1)
+    {
+    }
+}
+
+static void task1(void *args __attribute__((unused))){
+    while(true){
+        gpio_toggle(GPIOC, GPIO13);
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
 
 int main(void){
-    //se configura el clock del sistema a 72Mhz
-    rcc_clock_setup_in_hse_8mhz_out_72mhz();
+    rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
 
-    //se configura el led
     rcc_periph_clock_enable(RCC_GPIOC);
+
     gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+    //ENCENDEMOS el led
+    gpio_set(GPIOC, GPIO13);
+    //configuramos rtos task
+    xTaskCreate(task1,"LedSwitching",configMINIMAL_STACK_SIZE,NULL,tskLOW_PRIORITY,NULL);
+
+    vTaskStartScheduler();
 
     while(1){
-        for(int i=0;i<1000000;i++){
-            __asm__("nop");
-            gpio_toggle(GPIOC, GPIO13);
-        }
-    }
+
+    };
+    return 0;
 }
