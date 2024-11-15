bf428eb02fe264c5d2bf92fce0e851f3e86f62f6
Franco Mamani <franko15mamani@gmail.com>
2024-11-15 18:19:04 -0300

    Arreglo de formato y add configure_pins()

--- src/main.c
+++ src/main.c
@@ -8,47 +8,61 @@
 
 #include "lcd_i2c.h"
 
-//Definiciones de pins
-//TO DO: Agregar definiciones de los pines
+// Definiciones de pins
+// TO DO: Agregar definiciones de los pines
 
-//variables Globales
-volatile uint16_t temperatura=0;
-volatile uint16_t porcentajeBateria=0;
-uint8_t modo_sistema=0;
-uint16_t sensor_movimiento=0;
+// variables Globales
+volatile uint16_t temperatura = 0;
+volatile uint16_t porcentajeBateria = 0;
+uint8_t modo_sistema = 0;
+uint16_t sensor_movimiento = 0;
 
 char buffer_temp_bateria[32];
 char buffer_modo[32];
 
+#define tskLOW_PRIORITY ((UBaseType_t)tskIDLE_PRIORITY + 2)
 
-#define tskLOW_PRIORITY ((UBaseType_t)tskIDLE_PRIORITY+2)
-
-//rutina para controlar el stack overflow
-void vApplicationStackOverflowHook( TaskHandle_t pxTask __attribute__((unused)), char *pcTaskName __attribute__((unused))){
+// rutina para controlar el stack overflow
+void vApplicationStackOverflowHook(TaskHandle_t pxTask __attribute__((unused)), char *pcTaskName __attribute__((unused)))
+{
     while (1)
     {
     }
 }
 
-//asi se crean las tareas
-static void task1(void *args __attribute__((unused))){
-    while(true){
+void configure_pins()
+{
+    rcc_periph_clock_enable(RCC_GPIOC);
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+
+    //TODO: Agregar configuración de los pines
+}
+
+// asi se crean las tareas
+static void task1(void *args __attribute__((unused)))
+{
+    while (true)
+    {
         gpio_toggle(GPIOC, GPIO13);
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
-static void task_i2c(void *args __attribute__((unused))){
-    while(true){
+static void task_i2c(void *args __attribute__((unused)))
+{
+    while (true)
+    {
         temperatura++;
         porcentajeBateria++;
-        modo_sistema=0;
-        sprintf(buffer_temp_bateria,"Temperatura: %d",temperatura);
-        if(modo_sistema==0){
-            sprintf(buffer_modo,"Modo: Auto",modo_sistema);
+        modo_sistema = 0;
+        sprintf(buffer_temp_bateria, "Temperatura: %d", temperatura);
+        if (modo_sistema == 0)
+        {
+            sprintf(buffer_modo, "Modo: Auto", modo_sistema);
         }
-        else{
-            sprintf(buffer_modo,"Modo: Manual",modo_sistema);
+        else
+        {
+            sprintf(buffer_modo, "Modo: Manual", modo_sistema);
         }
         lcd_set_cursor(0, 0);
         lcd_print(buffer_temp_bateria);
@@ -58,29 +72,23 @@ static void task_i2c(void *args __attribute__((unused))){
     }
 }
 
-
-int main(void){
+int main(void)
+{
     rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
 
-    rcc_periph_clock_enable(RCC_GPIOC);
-  
     lcd_init();
 
-    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
-    //ENCENDEMOS el led
+    // ENCENDEMOS el led
     gpio_set(GPIOC, GPIO13);
 
+    // creamos las tareas
+    xTaskCreate(task1, "LedSwitching", configMINIMAL_STACK_SIZE, NULL, tskLOW_PRIORITY, NULL);
+    xTaskCreate(task_i2c, "I2C", configMINIMAL_STACK_SIZE, NULL, tskLOW_PRIORITY, NULL);
 
-    //creamos las tareas
-    xTaskCreate(task1,"LedSwitching",configMINIMAL_STACK_SIZE,NULL,tskLOW_PRIORITY,NULL);
-    xTaskCreate(task_i2c,"I2C",configMINIMAL_STACK_SIZE,NULL,tskLOW_PRIORITY,NULL);
-
-    //iniciamos todas las tareas
+    // iniciamos todas las tareas
     vTaskStartScheduler();
-  
-    
 
-    while(1){
-        
+    while (1)
+    {
     }
 }
