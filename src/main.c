3c83cd3292b990bac85f941a515591c6a707720b
Franco Mamani <franko15mamani@gmail.com>
2024-11-15 16:22:03 -0300

    agregamos comentarios

--- src/main.c
+++ src/main.c
@@ -6,14 +6,21 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
+//Definiciones de pins
+//TO DO: Agregar definiciones de los pines
+
+
 #define tskLOW_PRIORITY ((UBaseType_t)tskIDLE_PRIORITY+2)
 
+//rutina para controlar el stack overflow
 void vApplicationStackOverflowHook( TaskHandle_t pxTask __attribute__((unused)), char *pcTaskName __attribute__((unused))){
     while (1)
     {
     }
 }
 
+
+//asi se crean las tareas
 static void task1(void *args __attribute__((unused))){
     while(true){
         gpio_toggle(GPIOC, GPIO13);
@@ -29,9 +36,12 @@ int main(void){
     gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
     //ENCENDEMOS el led
     gpio_set(GPIOC, GPIO13);
-    //configuramos rtos task
+
+
+    //creamos las tareas
     xTaskCreate(task1,"LedSwitching",configMINIMAL_STACK_SIZE,NULL,tskLOW_PRIORITY,NULL);
 
+    //iniciamos todas las tareas
     vTaskStartScheduler();
 
     while(1){
