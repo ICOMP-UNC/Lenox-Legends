38880f8b340e0748c16ea48d8b426b12292ac6f2
franivan98 <franko15mamani@gmail.com>
2024-11-16 18:05:55 -0300

    Timer y FreeRTOS
    a traves del timer, levantamos la tarea task1 que hace parpadear al led del pin C13

--- src/main.c
+++ src/main.c
@@ -5,27 +5,15 @@
 #include <libopencm3/stm32/i2c.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/timer.h>
+#include <libopencm3/cm3/nvic.h>
 #include <stdint.h>
 #include <stdio.h>
 
 #include "lcd_i2c.h"
 
-// Definiciones de pins
-// TO DO: Agregar definiciones de los pines
-
-// variables Globales
-volatile uint16_t temperatura = 0;
-volatile uint16_t porcentajeBateria = 0;
-uint8_t modo_sistema = 0;
-uint16_t sensor_movimiento = 0;
-
-// SEMAFOROS
-xSemaphoreHandle temp_semaforo = 0;
-xSemaphoreHandle bateria_semaforo = 0;
-xSemaphoreHandle movimiento_semaforo = 0;
-
-char buffer_temp_bateria[32];
-char buffer_modo[32];
+//Creamos el taskHundle
+TaskHandle_t led_handle = NULL;
 
 #define tskLOW_PRIORITY ((UBaseType_t)tskIDLE_PRIORITY + 2)
 
@@ -38,13 +26,6 @@ void vApplicationStackOverflowHook(TaskHandle_t pxTask __attribute__((unused)),
     }
 }
 
-void inicializar_semaforos()
-{
-    temp_semaforo = xSemaphoreCreateMutex();
-    bateria_semaforo = xSemaphoreCreateMutex();
-    movimiento_semaforo = xSemaphoreCreateMutex();
-}
-
 void configure_pins()
 {
     rcc_periph_clock_enable(RCC_GPIOC);
@@ -53,73 +34,42 @@ void configure_pins()
     // TODO: Agregar configuración de los pines
 }
 
-void configure_usart(void)
+void configure_timer(void)
 {
-    rcc_periph_clock_enable(RCC_USART1); // Habilita USART1
-    rcc_periph_clock_enable(RCC_GPIOA);  // Habilita GPIOA para USART
-    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX); // Configura PA9 como TX
-    usart_set_baudrate(USART1, 9600);
-    usart_set_databits(USART1, 8);
-    usart_set_stopbits(USART1, USART_STOPBITS_1);
-    usart_set_mode(USART1, USART_MODE_TX);
-    usart_set_parity(USART1, USART_PARITY_NONE);
-    usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
-    usart_enable(USART1);
+    rcc_periph_clock_enable(RCC_TIM2); // Habilita el reloj para el Timer 2
+
+    timer_set_prescaler(TIM2, 7199); // Prescaler para un conteo de 10 kHz (72 MHz / 7200)
+    timer_set_period(TIM2, 9999);    // Periodo para generar interrupciones cada 1 segundo (10 kHz / 10000)
+
+    timer_enable_irq(TIM2, TIM_DIER_UIE); // Habilita la interrupción de actualización
+    nvic_enable_irq(NVIC_TIM2_IRQ);       // Habilita la interrupción del Timer 2 en el NVIC
+
+    timer_enable_counter(TIM2); // Inicia el contador del Timer 2
 }
 
-void usart_send_labeled_value(const char *label, uint16_t value)
-{
-    char buffer[20];
-    int len = snprintf(buffer, sizeof(buffer), ">%s:%u\n", label, value); // Formato: "Etiqueta:Valor"
-    for (int i = 0; i < len; i++)
-    {
-        usart_send_blocking(USART1, buffer[i]); // Envía cada carácter
-    }
-}
-
-// asi se crean las tareas
 static void task1(void *args __attribute__((unused)))
 {
     while (true)
     {
+        //Esperar la notificacion desde la ISR del timer
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+        
+        //notificacion recibida
         gpio_toggle(GPIOC, GPIO13);
-        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
-static void task_i2c(void *args __attribute__((unused)))
+void tim2_isr(void)
 {
-    while (true)
-    {
-        temperatura++;
-        porcentajeBateria++;
-        modo_sistema = 0;
-        sprintf(buffer_temp_bateria, "Temperatura: %d", temperatura);
-        if (modo_sistema == 0)
-        {
-            sprintf(buffer_modo, "Modo: Auto", modo_sistema);
-        }
-        else
-        {
-            sprintf(buffer_modo, "Modo: Manual", modo_sistema);
-        }
-        lcd_set_cursor(0, 0);
-        lcd_print(buffer_temp_bateria);
-        lcd_set_cursor(1, 0);
-        lcd_print(buffer_modo);
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
+    if (timer_get_flag(TIM2, TIM_SR_UIF))
+    {                                       // Verifica si la interrupción fue generada por el flag de actualización
+        timer_clear_flag(TIM2, TIM_SR_UIF); // Limpia el flag de interrupción de actualización
 
-static void task_uart(void *args __attribute__((unused)))
-{
-    while (true)
-    {
-        // Envía las variables etiquetadas por UART
-        usart_send_labeled_value("A1", temperatura); // Envía adc_value1 con etiqueta "A1"
-        usart_send_labeled_value("A2", porcentajeBateria); // Envía adc_value2 con etiqueta "A2"
-        usart_send_labeled_value("V3", sensor_movimiento); // Envía variable3_uart con etiqueta "V3"
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        BaseType_t xHigherPriorityTaskWoken = pdFALSE; //vble auxiliar. no se para que es
+        //despertamos a la tarea
+        vTaskNotifyGiveFromISR(led_handle, &xHigherPriorityTaskWoken);
+        //Forzar cambio de contexto si una tarea de mayor prioridad fue despertada.
+        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
     }
 }
 
@@ -127,22 +77,13 @@ int main(void)
 {
     rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
     configure_pins();
-    configure_usart();
-    lcd_init();
-
     // ENCENDEMOS el led
     gpio_set(GPIOC, GPIO13);
 
     // creamos las tareas
-    xTaskCreate(task1, "LedSwitching", configMINIMAL_STACK_SIZE, NULL,
-                tskLOW_PRIORITY, NULL);
-    xTaskCreate(task_i2c, "I2C", configMINIMAL_STACK_SIZE, NULL, tskLOW_PRIORITY,
-                NULL);
+    xTaskCreate(task1, "LedSwitching", configMINIMAL_STACK_SIZE, NULL, tskLOW_PRIORITY, &led_handle);
 
-    xTaskCreate(task_uart, "UART", configMINIMAL_STACK_SIZE, NULL, tskLOW_PRIORITY,
-                NULL);
-
-    // iniciamos todas las tareas
+    //iniciamos las tareas
     vTaskStartScheduler();
 
     while (1)
