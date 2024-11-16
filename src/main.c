ad2ea4de8e7efc4d888c6ccfc90495f82c19c0f3
franivan98 <franko15mamani@gmail.com>
2024-11-16 19:12:21 -0300

    c

--- src/main.c
+++ src/main.c
@@ -1,34 +1,79 @@
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
+#include "semphr.h"
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/i2c.h>
 #include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/usart.h>
 #include <libopencm3/stm32/timer.h>
 #include <libopencm3/cm3/nvic.h>
+#include <stdint.h>
+#include <stdio.h>
 
-void tim2_isr(void)
+#include "lcd_i2c.h"
+
+//Creamos el taskHundle
+TaskHandle_t led_handle = NULL;
+
+#define tskLOW_PRIORITY ((UBaseType_t)tskIDLE_PRIORITY + 2)
+
+// rutina para controlar el stack overflow
+void vApplicationStackOverflowHook(TaskHandle_t pxTask __attribute__((unused)),
+                                   char *pcTaskName __attribute__((unused)))
 {
-    if (timer_get_flag(TIM2, TIM_SR_UIF))
+    while (1)
     {
-        timer_clear_flag(TIM2, TIM_SR_UIF); // Limpia el flag
-        gpio_toggle(GPIOC, GPIO13);        // Alterna el LED
     }
 }
 
-void configure_pins(void)
+void configure_pins()
 {
     rcc_periph_clock_enable(RCC_GPIOC);
     gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+
+    // TODO: Agregar configuración de los pines
 }
 
 void configure_timer(void)
 {
-    rcc_periph_clock_enable(RCC_TIM2);
+    rcc_periph_clock_enable(RCC_TIM2); // Habilita el reloj para el Timer 2
 
-    timer_set_prescaler(TIM2, 7199);
-    timer_set_period(TIM2, 9999);
+    timer_set_prescaler(TIM2, 7199); // Prescaler para un conteo de 10 kHz (72 MHz / 7200)
+    timer_set_period(TIM2, 9999);    // Periodo para generar interrupciones cada 1 segundo (10 kHz / 10000)
 
-    timer_enable_irq(TIM2, TIM_DIER_UIE);
-    nvic_enable_irq(NVIC_TIM2_IRQ);
+    timer_enable_irq(TIM2, TIM_DIER_UIE); // Habilita la interrupción de actualización
+    nvic_enable_irq(NVIC_TIM2_IRQ);       // Habilita la interrupción del Timer 2 en el NVIC
+    nvic_set_priority(NVIC_TIM2_IRQ, 6);
+    timer_enable_counter(TIM2); // Inicia el contador del Timer 2
+    
+}
 
-    timer_enable_counter(TIM2);
+static void task1(void *args __attribute__((unused)))
+{
+    while (true)
+    {
+        //Esperar la notificacion desde la ISR del timer
+        //ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+        
+        //notificacion recibida
+        gpio_toggle(GPIOC, GPIO13);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void tim2_isr(void)
+{
+    if (timer_get_flag(TIM2, TIM_SR_UIF))
+    {                                       // Verifica si la interrupción fue generada por el flag de actualización
+        timer_clear_flag(TIM2, TIM_SR_UIF); // Limpia el flag de interrupción de actualización
+
+        BaseType_t xHigherPriorityTaskWoken = pdFALSE; //vble auxiliar. no se para que es
+        //despertamos a la tarea
+        vTaskNotifyGiveFromISR(led_handle, &xHigherPriorityTaskWoken);
+        //Forzar cambio de contexto si una tarea de mayor prioridad fue despertada.
+        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+    }
 }
 
 int main(void)
@@ -36,9 +81,16 @@ int main(void)
     rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
     configure_pins();
     configure_timer();
+    // ENCENDEMOS el led
+    gpio_set(GPIOC, GPIO13);
+
+    // creamos las tareas
+    xTaskCreate(task1, "LedSwitching", configMINIMAL_STACK_SIZE, NULL, tskLOW_PRIORITY, &led_handle);
+
+    //iniciamos las tareas
+    vTaskStartScheduler();
 
     while (1)
     {
-        // Bucle principal vacío
     }
 }
