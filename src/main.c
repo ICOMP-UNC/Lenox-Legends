0c8b7c1a5b7209c28ce74bfd09dce3e0f51aa568
Lenox30 <lenox.graham@mi.unc.edu.ar>
2024-11-18 22:50:02 -0300

    Timer funcionando

--- src/main.c
+++ src/main.c
@@ -8,7 +8,9 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/usart.h>
 #include <libopencm3/cm3/systick.h>
+#include <libopencm3/stm32/timer.h>
 #include <libopencm3/cm3/nvic.h>
+#include <libopencm3/stm32/adc.h>
 #include <stdint.h>
 #include <stdio.h>
 #include "lcd_i2c.h"
@@ -16,6 +18,8 @@
 //defines
 #define RELOAD_COUNT 8999
 #define TOGGLE_COUNT 500
+#define TIMER_PRESCALER 7199 //prescaler para 10khz
+#define TIMER_PERIOD 9999 //periodo para 1khz
 
 //magic numbers
 #define BUFFER_SIZE 16
@@ -82,12 +86,31 @@ void configure_systick(void){
     systick_counter_enable();
 }
 
-void sys_tick_handler(void){
-    systick_Count++;
-    if(systick_Count > TOGGLE_COUNT){
-        systick_Count = 0;
-        gpio_toggle(GPIOC, GPIO13);
-        print_lcd();
+void configure_timer(void) {
+  rcc_periph_clock_enable(RCC_TIM2);       // Habilita el reloj para el Timer 2
+
+  timer_set_prescaler(TIM2, TIMER_PRESCALER);         // Prescaler para un conteo de 10 kHz (72 MHz / 7200)
+  timer_set_period(TIM2, TIMER_PERIOD);            // Periodo para generar interrupciones cada 1 segundo (10 kHz / 10000)
+
+  timer_enable_irq(TIM2, TIM_DIER_UIE);    // Habilita la interrupción de actualización
+  nvic_enable_irq(NVIC_TIM2_IRQ);          // Habilita la interrupción del Timer 2 en el NVIC
+
+  timer_enable_counter(TIM2);              // Inicia el contador del Timer 2
+}
+
+
+
+
+int main(void)
+{
+    systemInit();
+    lcd_init();
+    configure_pins();
+    configure_systick();
+    configure_timer();
+    while (1)
+    {
+        
     }
 }
 
@@ -105,14 +128,22 @@ void print_lcd(){
     lcd_print(buffer);
 }
 
-int main(void)
-{
-    systemInit();
-    lcd_init();
-    configure_pins();
-    configure_systick();
-    while (1)
-    {
-        
+void sys_tick_handler(void){
+    systick_Count++;
+    if(systick_Count > TOGGLE_COUNT){
+        systick_Count = 0;
+       // gpio_toggle(GPIOC, GPIO13);
+        print_lcd();
     }
 }
+
+void tim2_isr(void) {
+  if (timer_get_flag(TIM2, TIM_SR_UIF)) {   // Verifica si la interrupción fue generada por el flag de actualización
+    timer_clear_flag(TIM2, TIM_SR_UIF);     // Limpia el flag de interrupción de actualización
+
+    //gpio_toggle(GPIOC, GPIO13);             // Cambia el estado del pin PC13
+
+  }
+}
+
+
