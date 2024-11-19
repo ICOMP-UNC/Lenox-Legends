dd5d6d14cc23a14e22a6695b8455417bc60caefe
Lenox30 <lenox.graham@mi.unc.edu.ar>
2024-11-18 23:31:37 -0300

    ADC con 2 canales funcionando

--- src/main.c
+++ src/main.c
@@ -20,6 +20,7 @@
 #define TOGGLE_COUNT 500
 #define TIMER_PRESCALER 7199 //prescaler para 10khz
 #define TIMER_PERIOD 9999 //periodo para 1khz
+#define BATERIA_TIMER 2 //1Leeremos la bateria cada 10 seg;
 
 //magic numbers
 #define BUFFER_SIZE 16
@@ -28,6 +29,8 @@
 //variables globales
 volatile uint32_t systick_Count=0;
 volatile uint16_t temperatura=0;
+volatile uint16_t bateria=0;
+volatile uint8_t Timer_Batery_Count=0;  //contador para leer la bateria
 
 char buffer[BUFFER_SIZE];
 
@@ -75,7 +78,7 @@ void configure_pins()
 {
     rcc_periph_clock_enable(RCC_GPIOC);
     gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
-
+    gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO0 | GPIO1);
     // TODO: Agregar configuración de los pines
 }
 
@@ -98,7 +101,30 @@ void configure_timer(void) {
   timer_enable_counter(TIM2);              // Inicia el contador del Timer 2
 }
 
+void configure_adc(void) {
+  rcc_periph_clock_enable(RCC_ADC1);  // Habilita el reloj para el ADC1
 
+  adc_power_off(ADC1);  // Apaga el ADC para configurarlo
+
+  // Configura el tiempo de muestreo para cada canal
+  adc_set_sample_time(ADC1, ADC_CHANNEL0, ADC_SMPR_SMP_1DOT5CYC);  // Canal 0
+  adc_set_sample_time(ADC1, ADC_CHANNEL1, ADC_SMPR_SMP_1DOT5CYC);  // Canal 1
+
+  // Configura la secuencia regular de canales
+
+  // Habilita la interrupción de fin de conversión (EOC) del ADC
+  adc_enable_eoc_interrupt(ADC1);
+  nvic_enable_irq(NVIC_ADC1_2_IRQ);  // Habilita la interrupción del ADC en el NVIC
+
+  // Configura el ADC para que dispare la transferencia de datos
+  adc_enable_dma(ADC1);  // Habilita DMA (si es necesario)
+
+  adc_power_on(ADC1);  // Enciende el ADC
+
+  adc_reset_calibration(ADC1);  // Reinicia la calibración
+  adc_calibrate(ADC1);  // Calibra el ADC
+ 
+}
 
 
 int main(void)
@@ -108,6 +134,7 @@ int main(void)
     configure_pins();
     configure_systick();
     configure_timer();
+    configure_adc();
     while (1)
     {
         
@@ -140,10 +167,37 @@ void sys_tick_handler(void){
 void tim2_isr(void) {
   if (timer_get_flag(TIM2, TIM_SR_UIF)) {   // Verifica si la interrupción fue generada por el flag de actualización
     timer_clear_flag(TIM2, TIM_SR_UIF);     // Limpia el flag de interrupción de actualización
+    adc_disable_scan_mode(ADC1);
+    if(Timer_Batery_Count==BATERIA_TIMER){
+      adc_set_regular_sequence(ADC1, 1, ADC_CHANNEL1);
+      adc_start_conversion_direct(ADC1);
+    }
+    else{
+        adc_set_regular_sequence(ADC1, 1, ADC_CHANNEL0);
+        adc_start_conversion_direct(ADC1);
 
-    //gpio_toggle(GPIOC, GPIO13);             // Cambia el estado del pin PC13
+    }
 
   }
 }
 
+void adc1_2_isr(void) {
+  if (adc_eoc(ADC1)) {  // Verifica si la conversión ha finalizado
+     if(Timer_Batery_Count==BATERIA_TIMER){
+      bateria = adc_read_regular(ADC1);  // Lee el valor de la conversión
+      Timer_Batery_Count=0;
+     }
+     else{
+      Timer_Batery_Count++;
+      temperatura = adc_read_regular(ADC1);  // Lee el valor de la conversión
+     }
 
+  }
+
+  if(bateria>2000){
+    gpio_set(GPIOC, GPIO13);
+  }
+  else{
+    gpio_clear(GPIOC, GPIO13);
+  }
+}
